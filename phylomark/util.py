907b697f113914893b3f4f3581eaa54183820968
Jason Sahl <jsahl@tgen.org>
2015-07-21 13:00:12 -0700

    working

--- phylomark/util.py
+++ phylomark/util.py
@@ -255,7 +255,7 @@ def tree_loop(fastadir, combined, tree, parallel_workers, run_r, num_refs):
         thread_name_file = str(thread_id) + '_name.txt'
         write_strip_name(f, thread_name_file)
         polys_name_file = str(thread_id) + '_polys.txt'
-        parse_poly_file(_temp_name(tn, "polys.txt"), polys_name_file))
+        parse_poly_file(_temp_name(tn, "polys.txt"), polys_name_file)
         subprocess.check_call(["rm",
                                _temp_name(tn, "blast_parsed.txt"),
                                _temp_name(tn, "blast_unique.parsed.txt"),
